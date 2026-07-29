23d62a380676ff7edf1a0b711a48f2f9712e1df7
alex-no <alex@4n.com.ua>
2026-07-29 21:42:40 +0300

    build: staging deploy 2026-07-29 21:42

--- backend/public/app-assets/Bookings-DbxjhC2h.js
+++
@@ -1,1 +0,0 @@
-import{O as e,P as t,g as n,h as r}from"./index-SQnnzsl6.js";import{t as i}from"./BaseLayout-CtCktYRs.js";var a={__name:`Bookings`,setup(a){return(a,o)=>(e(),n(i,null,{default:t(()=>[...o[0]||=[r(`h5`,{class:`mb-3`},`Записи`,-1),r(`p`,{class:`text-muted`},`В розробці…`,-1)]]),_:1}))}};export{a as default};
