2924a3b7752ff7d3c2a47f10a27d484e98457217
alex-no <alex@4n.com.ua>
2026-08-09 13:07:26 +0300

    Remove some extra pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,31 +10,8 @@ const routes = [
   { path: '/dashboard', name: 'Dashboard', component: () => import('../pages/Dashboard.vue') },
   { path: '/sto',          name: 'StoList',     component: () => import('../pages/StoList.vue')     },
   { path: '/data-registry', name: 'DataRegistry', component: () => import('../pages/StoRegistry.vue') },
-  { path: '/sto/import',        name: 'StoImport',        component: () => import('../pages/StoImport.vue')        },
-  { path: '/sto/outreach',      name: 'StoOutreach',      component: () => import('../pages/StoOutreach.vue')      },
-  { path: '/sto/applications', name: 'StoApplications', component: () => import('../pages/StoApplications.vue') },
-  { path: '/bookings',  name: 'Bookings',  component: () => import('../pages/Bookings.vue')  },
-  { path: '/users',     name: 'Users',     component: () => import('../pages/Users.vue')     },
-  { path: '/sto-managers', name: 'StoManagers', component: () => import('../pages/StoManagers.vue') },
-  { path: '/admin-management', name: 'AdminManagement', component: () => import('../pages/AdminManagement.vue') },
   { path: '/roles', name: 'RoleManagement', component: () => import('../pages/RoleManagement.vue') },
   { path: '/permissions', name: 'PermissionList', component: () => import('../pages/PermissionList.vue') },
-
-  // Geography
-  { path: '/geography/countries',   name: 'Countries',  component: () => import('../pages/geography/Countries.vue')  },
-  { path: '/geography/areas',       name: 'Areas',      component: () => import('../pages/geography/Areas.vue')      },
-  { path: '/geography/districts',   name: 'Districts',  component: () => import('../pages/geography/Districts.vue')  },
-  { path: '/geography/cities',      name: 'Cities',     component: () => import('../pages/geography/Cities.vue')     },
-  { path: '/geography/city-types',       name: 'CityTypes',      component: () => import('../pages/geography/CityTypes.vue')      },
-  { path: '/geography/city-tmp-review', name: 'CityTmpReview', component: () => import('../pages/geography/CityTmpReview.vue') },
-  { path: '/geography/timezones',       name: 'Timezones',    component: () => import('../pages/geography/Timezones.vue')    },
-
-  // Catalog
-  { path: '/catalog/vehicle-types', name: 'VehicleTypes', component: () => import('../pages/catalog/VehicleTypes.vue') },
-  { path: '/catalog/car-brands',    name: 'CarBrands',    component: () => import('../pages/catalog/CarBrands.vue')    },
-  { path: '/catalog/car-models',    name: 'CarModels',    component: () => import('../pages/catalog/CarModels.vue')    },
-  { path: '/catalog/service-groups', name: 'ServiceGroups', component: () => import('../pages/catalog/ServiceGroups.vue') },
-  { path: '/catalog/services',       name: 'Services',      component: () => import('../pages/catalog/Services.vue')      },
 
   // System / Error Logs
   { path: '/error-logs',       name: 'ErrorLogs',      component: () => import('../pages/ErrorLogs.vue')      },
@@ -45,17 +22,9 @@ const routes = [
   { path: '/analytics/stats',      name: 'AnalyticsStats',      component: () => import('../pages/AnalyticsStats.vue')      },
   { path: '/analytics/charts',     name: 'AnalyticsCharts',     component: () => import('../pages/AnalyticsCharts.vue')     },
 
-  // Feedback
-  { path: '/feedback', name: 'Feedback', component: () => import('../pages/Feedback.vue') },
-  { path: '/feedback/stats', name: 'FeedbackStats', component: () => import('../pages/FeedbackStats.vue') },
-
   // Test pages
   { path: '/test-facets', name: 'TestFacets', component: () => import('../pages/TestFacets.vue') },
   { path: '/test-i18n', name: 'TestI18n', component: () => import('../pages/TestI18n.vue') },
-  { path: '/news', name: 'News', component: () => import('../pages/News.vue') },
-
-  // Reviews
-  { path: '/reviews', name: 'Reviews', component: () => import('../pages/Reviews.vue') },
 
   // Dev Tools
   { path: '/api-docs-admin', name: 'ApiDocsAdmin', component: () => import('../pages/ApiDocsAdmin.vue') },
